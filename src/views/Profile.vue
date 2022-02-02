9fd78f4d696c8327549c8dc0c7bf167594a7114f
Sainrahmani12 <husainrahmani127@gmail.com>
2022-02-02 09:52:26 +0700

    first commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="profile">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <img src="../assets/earth.webp" class="user-pic mr-3"/>
+            </div>
+            <div class="flex-grow-1 ms-3">
+              <h5>{{ lastname }} {{ firstname }} {{ tmbhn }} </h5>
+              {{ isi }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "profile",
+  data: function() {
+    return {
+      firstname: "Husain",
+      lastname: "Rahmani",
+      tmbhn: "El-Hamidy",
+      isi : "Barakallahu lakuma , wajama'a baynakuma bilkhair."
+    }
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.user-pic{
+  border-radius: 200%;
+  height: 120px;
+  width: 120px;
+}
+</style>
